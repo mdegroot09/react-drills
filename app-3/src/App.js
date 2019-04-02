2e7378959a532406265fb9643f47b461cd5cbe62
Mike De Groot <mdegroot09@gmail.com>
2019-04-02 15:38:43 -0600

    app 3 complete

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -3,16 +3,32 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      filterString: '',
+      array: [
+        'movies', 'restaurants', 'bowling', 'swimming', 'sports'
+      ]
+    }
+  }
+
+  handleChange(val){
+    this.setState({filterString: val})
+  }
+  
   render() {
+    let activities = this.state.array.filter((activity, index) => {
+      return activity.includes(this.state.filterString)
+    })
+    activities = activities.map((activity, index) => {
+      return <h2 key={index}>{activity}</h2>
+    })
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={(event) => {this.handleChange(event.target.value)}} type='text'></input>
+        {activities}
       </div>
     );
   }
